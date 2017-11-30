9ccfd5bb326567f5f0be13e255feab2530c6cae3
Mickey J Winters <www.w.com123@gmail.com>
2017-11-30 01:14:29 -0500

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,9 +1,9 @@
-function FIX_THIS_S___(){
+function fixFooter(){
     var contentHeight=$("nav").outerHeight()+$("section").outerHeight();
     if(contentHeight<window.innerHeight){
 	var footerY=window.innerHeight-$("footer").outerHeight()
 	$("footer").offset({top:footerY});
     }
 }
-$(window).on("resize", FIX_THIS_S___);
-$("footer").ready(FIX_THIS_S___);
+$(window).on("resize", fixFooter);
+$("footer").ready(fixFooter);
