faed821dc82de3e408a79c2957efbebd3ce3ac90
Biggie Emmanuel <bigolu96@gmail.com>
2018-03-10 20:20:09 -0500

    add some styling + more responsive + make map a heatmap

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,4 +1,5 @@
 /*
+ *
  * Map for where RU students/alumni currently are working/interning.
  *
  * Much thanks to these resources:
@@ -15,25 +16,21 @@ const SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/1HgouuYpc9auDTOL
 // url for map.
 const MAP_JSON_URL = "assets/map/us-states-quantized-topo.json";
 
-// The width that the map was initially projected to. A ratio of this
-// to the current window width defines the scale at which the map should be
-// drawn,
+// The dimensions that the map was initially projected to. A ratio of this
+// to the current window dimensions defines the scale at which the
+// map should be drawn.
 const MAP_BASE_WIDTH = 750;
+const MAP_BASE_HEIGHT = 400;
 
 // Minimum time (in ms) required before map is redrawn.
 const REDRAW_THROTTLE_TIME = 300;
-
-// A list of geoJSON feautures for each state.
-var states = null;
-
-// d3 selection for svg element of map.
-var svg = null;
 
 /*
  * Returns the path attribute for the map's <path> elements.
  */
 function getMapPath(width, height) {
-    const scale = width / MAP_BASE_WIDTH;
+    const scale = (width < height) ?
+        width / MAP_BASE_WIDTH : height / MAP_BASE_HEIGHT;
     const area = 1;
 
     const simplify = d3.geo.transform({
@@ -51,15 +48,42 @@ function getMapPath(width, height) {
         }});
 }
 
-/*
- * Draws the map.
+/* 
+ * Returns dimensions of map based on current window dimensions.
  */
-function draw() {
-    const width = $('#map-entry-point').outerWidth() - 370;
-    const height = (width / 2) + 50;
+function getNewMapDimensions() {
+    const width = $('#map-container').width() * .75;
+    const height = $("#map-container").height() - $("#map-heading").height();
+
+    return [width, height];
+}
+
+/*
+ * Resize the map based on new window dimensions
+ */
+function resize() {
+    const [width, height] = getNewMapDimensions();
+    const path = getMapPath(width, height);
+
+    // set map width/height
+    d3.select("#map").attr("width", width).attr("height", height);
+
+    // update 'd' attribute for all state <path>'s
+    d3.select("#map g").selectAll(".state").attr("d", path);
+}
+
+/*
+ * Draws the map. Should only be called once on document.ready()
+ */
+function draw(states) {
+    const [width, height] = getNewMapDimensions();
     const tooltip = d3.select(".tooltip");
     const path = getMapPath(width, height);
+    const svg = d3.select("#map");
     const statesGroup = svg.select("g");
+
+    // reduce the amount of times we have to redraw the map
+    d3.select(window).on("resize", throttleRedraw);
 
     // set map width/height and define pan/zoom behavior
     svg.attr("width", width)
@@ -73,16 +97,15 @@ function draw() {
                 .attr("transform", `translate(${translate}) scale(${scale})`);
         }));
 
-    // update state elements in map
-    const statesUpdate = statesGroup.selectAll(".state").data(states);
-    statesUpdate.attr("d", path);
-
     // add new state elements to map
     let statesEnter = statesGroup.selectAll(".state").data(states).enter();
     statesEnter = statesEnter.append("a").attr("href", "#map-listing")
         .append("path")
         .attr("class", "state")
-        .attr("d", path);
+        .attr("d", path)
+        .attr("style", function(state, i) {
+            return `fill: ${state.properties['bgcolor']};`
+        });
 
     // define interactivity for new state elements in map
     statesEnter
@@ -90,8 +113,10 @@ function draw() {
             const stateName = state.properties['NAME10'];
 
             // toggle selected state
-            statesGroup.select(".selected-state").classed("selected-state", false);
-            d3.select(this).classed("selected-state", true);
+            statesGroup.select(".selected-state")
+                .classed("selected-state", false);
+            d3.select(this).
+                classed("selected-state", true);
 
             // update state name on page
             d3.select("#state-name").html(stateName);
@@ -102,11 +127,13 @@ function draw() {
                 .data(state.properties.people);
 
             // update existing list elements
-            people.text(function(person, i) {return person.name});
+            people.classed("list-group-item", true)
+                .text(function(person, i) {return person.name});
 
             // add new list elements
             people.enter()
                 .append("li")
+                .classed("list-group-item", true)
                 .text(function(person, i) {return person.name});
 
             // remove any excess list elements
@@ -114,14 +141,16 @@ function draw() {
 
             // add placeholder text to list, if noone is working in that state
             if (!d3.select("#map-listing").html()) {
-                d3.select("#map-listing").append("li").html("Looks like noone is working here yet!");
+                d3.select("#map-listing")
+                    .append("li")
+                    .html("Looks like noone is working here yet!");
             }
         })
         .on("mousemove", function(state,i) {
             const stateName = state.properties['NAME10'];
 
             // get mouse position
-            const mouse = d3.mouse(svg.node()).map( function(d) { return parseInt(d); } );
+            const mouse = d3.mouse(svg.node()).map((d) => parseInt(d));
 
             // offsets for tooltip relative to mouse position
             const offsetL = $('#map-entry-point').offset()['left'] + (width/30);
@@ -149,13 +178,11 @@ function draw() {
  */
 function throttleRedraw() {
     if (typeof  throttleRedraw.timer === 'undefined') {
-        throttleRedraw.timer = window.setTimeout(() => draw(),
-            REDRAW_THROTTLE_TIME);
+        throttleRedraw.timer = window.setTimeout(resize, REDRAW_THROTTLE_TIME);
     }
 
     window.clearTimeout(throttleRedraw.timer);
-    throttleRedraw.timer = window.setTimeout(() => draw(),
-        REDRAW_THROTTLE_TIME);
+    throttleRedraw.timer = window.setTimeout(resize, REDRAW_THROTTLE_TIME);
 }
 
 /* 
@@ -180,6 +207,7 @@ function populateStates(states) {
     return new Promise((resolve, reject) => {
         const query = new google.visualization.Query(SPREADSHEET_URL);
         query.setQuery("SELECT *");
+
         query.send((resp) => {
             if (resp.isError()) reject(resp.getDetailedMessage());
 
@@ -206,7 +234,23 @@ function populateStates(states) {
                 stateMap[stateName].push(person);
             }
 
-            resolve();
+            // store bg-color per state for heatmap
+            const numPeoplePerState = states.map((state) => state.properties['people'].length);
+            const min = Math.min.apply(Math, numPeoplePerState);
+            const max = Math.max.apply(Math, numPeoplePerState);
+            const range = max - min;
+            const stateColors = numPeoplePerState.map((numPeople) => {
+                const colorGradientRange = 230;
+                const normalizedVal = (numPeople - min) / range;
+                const colorGradientVal = Math.round(colorGradientRange * normalizedVal);
+
+                return `rgb(255, ${colorGradientRange - colorGradientVal}, 0)`;
+            });
+            for (let i = 0; i < stateColors.length; i++) {
+                states[i].properties['bgcolor'] = stateColors[i];
+            }
+
+            resolve(states);
         });
     });
 }
@@ -215,21 +259,9 @@ function populateStates(states) {
  * Init map
  */
 $(document).ready(() => {
-    // reduce the amount of times we have to redraw the map
-    d3.select(window).on("resize", throttleRedraw);
-
-    // selection for map element
-    svg = d3.select("#map");
-
-    getStates().then((val) => {
-        states = val;
-        populateStates(states).then(() =>
-            draw()
-        ).catch((err) => {
-            console.log(err);
-        });
-    }).catch((err) => {
-        console.log(err);
-    });
+    getStates()
+        .then(populateStates)
+        .then(draw)
+        .catch(console.log);
 });
 
