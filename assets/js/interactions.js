f1cb9622e7df6e9a073b722940e5197eb8372e82
Alexander Goodkind <alexander@goodkind.io>
2018-09-27 19:17:01 -0400

    added mentors page still needs masonry

--- assets/js/interactions.js
+++
@@ -1,9 +0,0 @@
-$(window).on("load resize scroll", function(e) {
-    $('.active-wrap').css('height', $('.active-area').first().innerHeight());
-    $('.in-page-link').bind('click', function(e) {
-        $(this).addClass('current');
-        $('.in-page-link').not(this).removeClass('current');
-        $('.active-wrap').find('.current').removeClass('current');
-        $('.active-wrap').find($(this).attr('href')).addClass('current');
-    });
-});
