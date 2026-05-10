f1453ef3a74ce78552a242216aeb5bbd37e8cf71
Hasan Ali <hasanali5103@gmail.com>
2026-05-10 17:25:33 -0400

    now deleted base path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Enables static export to an /out directory
-  basePath: '/usacs.github.io', // Matches your GitHub repository name
+  basePath: '', // Matches your GitHub repository name
   images: {
     unoptimized: true, // Required as GitHub Pages doesn't support the Next.js Image Optimization API
   },
