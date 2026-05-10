955540584ce287cdaa071aa1c932c5fee35fe965
Pallavi Biswas <64264921+pallavibiswas@users.noreply.github.com>
2026-05-10 15:57:06 -0400

    env var

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export', // Enables static export to an /out directory
+  basePath: '/usacs.github.io', // Matches your GitHub repository name
+  images: {
+    unoptimized: true, // Required as GitHub Pages doesn't support the Next.js Image Optimization API
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
